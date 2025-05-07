45e5d8e45d76e0e189fefb347b243a2d0575d962
ANDRE DE LIMA MIRANDA BORGES <andre.borges@sou.unifeob.edu.br>
2025-05-06 21:17:31 -0300

    Update home_page.dart

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,29 @@
-import 'package:app_nivardo/src/pages/cadastro_page.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int count = 0;
+
+  void incremented() {
+    setState(() {
+      count++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      count--;
+    });
+  }
+
+  bool get isEmpty => count == 0;
+  bool get isFull => count == 10;
 
   @override
   Widget build(BuildContext context) {
@@ -10,73 +31,74 @@ class HomePage extends StatelessWidget {
       body: Container(
         alignment: Alignment.center,
         decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/img_fundo.png'),
-                fit: BoxFit.cover)),
+          image: DecorationImage(
+            image: AssetImage('assets/images/img_fundo.png'),
+            fit: BoxFit.cover,
+          ),
+        ),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text(
-              'Acesso Liberado',
+            Text(
+              isFull ? 'Acesso Bloqueado' : 'Acesso Liberado',
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold),
+                color: isFull ? Color(0xFFFF0000) : Color(0xFFFFFFFF),
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+              ),
             ),
-            const Text(
-              '0',
+            Text(
+              count.toString(),
               style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold),
+                color: Color(0xFFFFFFFF),
+                fontSize: 60,
+                fontWeight: FontWeight.w500,
+              ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.amber,
-                      fixedSize: const Size(110, 75),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      )),
-                  onPressed: () {},
+                    backgroundColor:
+                        isEmpty ? Color(0xAAAFAE24) : Color(0xFFAFAE24),
+                    fixedSize: const Size(110, 75),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  onPressed: isEmpty ? () {} : decrement,
                   child: const Text(
                     'Sair',
                     style: TextStyle(
                       fontSize: 20,
-                      color: Colors.black,
+                      color: Color(0xFF031CF5),
                       fontWeight: FontWeight.w500,
                     ),
                   ),
                 ),
-                SizedBox(
-                  width: 40,
-                ),
+                const SizedBox(width: 40),
                 ElevatedButton(
                   style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color(0xFFFFC107),
-                      fixedSize: const Size(110, 75),
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(24),
-                      )),
-                  onPressed: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => CadastroScreen()),
-                    );
-                  },
+                    backgroundColor:
+                        isFull ? Color(0xAAAFAE24) : Color(0xFFAFAE24),
+                    fixedSize: const Size(110, 75),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(24),
+                    ),
+                  ),
+                  onPressed: isFull ? () {} : incremented,
                   child: const Text(
                     'Entrar',
                     style: TextStyle(
                       fontSize: 20,
-                      color: Colors.black,
+                      color: Color(0xFF031CF5),
                       fontWeight: FontWeight.w500,
                     ),
                   ),
-                )
+                ),
               ],
-            )
+            ),
           ],
         ),
       ),
