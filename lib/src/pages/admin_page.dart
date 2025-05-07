f490e896af34f441ed1b58b96403d17151f2ad7f
ANDRE DE LIMA MIRANDA BORGES <andre.borges@sou.unifeob.edu.br>
2025-05-06 21:16:07 -0300

    Update admin_page.dart

--- lib/src/pages/admin_page.dart
+++ lib/src/pages/admin_page.dart
@@ -1,65 +1,102 @@
 import 'package:flutter/material.dart';
-
-void main() {
-  runApp(MaterialApp(home: AdminPage()));
-}
+import 'users_page.dart'; // certifique-se de que o caminho está certo
 
 class AdminPage extends StatelessWidget {
+  const AdminPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Admin'),
-        backgroundColor: Colors.grey[800],
+        centerTitle: true,
+        backgroundColor: const Color(0xFF000000),
+        foregroundColor: const Color(0xFFFFFFFF),
+        title: const Text('Fluxo Livre - ADMIN'),
       ),
-      body: GridView.count(
-        crossAxisCount: 2,
-        children: [
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.grey[300],
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.people, size: 50),
-                Text('Usuários'),
-              ],
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: GridView.count(
+          crossAxisCount: 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10,
+          children: [
+            AdminGridItem(
+              title: 'Usuários',
+              image: 'assets/images/usuarios.png',
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (_) => const UsersPage()),
+                );
+              },
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.grey[300],
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.bar_chart, size: 50),
-                Text('Relatórios'),
-              ],
+            const AdminGridItem(
+              title: 'Relatório',
+              image: 'assets/images/relatorio.png',
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.grey[300],
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.settings, size: 50),
-                Text('Configurações'),
-              ],
+            const AdminGridItem(
+              title: 'Configurações',
+              image: 'assets/images/gear.png',
             ),
-          ),
-          Container(
-            margin: EdgeInsets.all(10),
-            color: Colors.grey[300],
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.exit_to_app, size: 50),
-                Text('Sair'),
-              ],
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class AdminGridItem extends StatelessWidget {
+  final String title;
+  final String image;
+  final VoidCallback? onTap;
+
+  const AdminGridItem({
+    super.key,
+    required this.title,
+    required this.image,
+    this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(12),
+          color: Colors.grey.shade300,
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.shade600,
+              blurRadius: 4,
+              offset: const Offset(2, 2),
             ),
-          ),
-        ],
+          ],
+        ),
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: [
+            Center(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Image.asset(image),
+              ),
+            ),
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.all(8.0),
+              color: Colors.black45,
+              child: Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
